1ff0f34a9982a8bc42621556711ac8e81789b3c9
abishek07bimali <abi0shekbimali@gmail.com>
2026-05-11 14:14:45 +0545

    project init

--- app.py
+++ app.py
@@ -1,1 +1,10 @@
-print("hello wor;ld!")
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "Hello, World! This is my first Flask website."
+
+if __name__ == "__main__":
+    app.run(debug=True)
